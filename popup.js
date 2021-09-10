fb5233ac538d70561bee203d6ed0b54e3728b413
subinkim26 <dpaptlove@gmail.com>
2021-09-10 17:41:24 +0900

    투두리스트 추가, 삭제, 리셋 작업
    추가 버튼 성공
    데이터 저장 성공
    삭제 성공
    리셋, 부분삭제 작업중

---
+++ popup.js
@@ -0,0 +1,135 @@
+const toDoForm = document.querySelector('.input');
+let items = document.querySelector('.list');
+let itemPlus = document.getElementById('.todo_add');
+let deleteBtns = document.querySelector('.delete');
+let reset = document.querySelector('.reset')
+const toDoInput = toDoForm.querySelector("input");
+
+
+document.querySelector('ul').addEventListener('click',checkToDo);
+reset.addEventListener('click', resetToDo);
+toDoForm.addEventListener("submit", handleSubmit);
+loadToDos();
+
+
+const TODOS_Ls = "toDos";
+let toDos = [];
+
+function add (text){
+
+
+    let itemElement = document.createElement('li'); //태그
+    itemElement.classList.add('item');
+
+    
+    let checkTag = document.createElement('input');
+    checkTag.setAttribute('type','checkbox');
+    checkTag.classList.add('item--check');
+
+    const span = document.createElement('span'); // span 태그생성
+    const newId = toDos.length + 1;
+
+    let removeTag = document.createElement('button');
+    removeTag.classList.add('list--remove');
+    const x = document.createTextNode("X");
+    span.innerText = text;
+    removeTag.appendChild(x);
+    itemElement.appendChild(checkTag);
+    itemElement.appendChild(span);
+    //itemElement.appendChild(textTag);
+    itemElement.appendChild(removeTag);
+    itemElement.id= newId;
+    items.appendChild(itemElement);
+  
+    removeTag.addEventListener('click', deleteToDo);
+
+   const toDoObj = { // toDos 배열안에 넣을 정보를 setup 
+        text,
+        id: newId
+    };
+    
+    toDos.push(toDoObj);
+    saveToDos()
+    
+
+    
+    console.log(toDos);
+   
+    }
+
+function handleSubmit(e) {
+    e.preventDefault(); // 이벤트가 작동하지 못하게 함
+    const currentValue = toDoInput.value; // currentValue에 input창에 입력한 값 대입
+    add(currentValue); // 리스트 추가하는 함수
+    toDoInput.value = ""; // input창 초기화
+    toDoInput.focus();
+  }
+
+
+
+function saveToDos(){
+    localStorage.setItem(TODOS_Ls, JSON.stringify(toDos));
+}
+
+deleteBtns.addEventListener('click', e=>{
+
+        var checkboxs = document.querySelectorAll('.item--check:checked');
+        var checkTo = checkboxs.forEach(item => item.parentElement.remove());
+        toDos = checkTo;
+        saveToDos();
+           
+    
+});
+
+function deleteToDo(e){
+    const btn = e.target;
+    let li = btn.parentElement;
+    items.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+       return toDo.id !==parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function resetToDo(e){
+    let ul = document.querySelector('.list').innerHTML = '';
+    toDos = ul;
+    saveToDos();
+
+}
+
+function checkToDo(e){
+    const todo = e.target.nextSibling;
+    if(e.target.checked){
+        todo.style.color = "#dddddd";
+        todo.style.textDecoration = "line-through";
+
+    }else{
+        todo.style.color = "#000000";
+        todo.style.textDecoration = "none"
+    }
+   saveToDos();
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_Ls);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos); // loadedToDos를 json객체로 변경
+        parsedToDos.forEach(function (toDo) { // 객체 내용 한개씩 파라미터로 넣어서 함수 실행
+          add(toDo.text); // 리스트 추가하는 함수
+        });
+      }
+
+}
+
+
+window.addEventListener('keydown',e=>{
+    if(e.keyCode !== 13) {
+        e.stopPropagation();
+        //saveToDos();
+        return;
+    }
+    //itemPlus.click();
+
+})
